74bd87c20102861c3e41465a335b4b3bad842dc7
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-11-21 12:11:32 -0600

    open in new tab

--- app/components/socials.tsx
+++ app/components/socials.tsx
@@ -6,13 +6,13 @@ import React from 'react'
 export const Socials = () => {
   return (
     <div className='flex gap-5'>
-      <Link href='https://linkedin.com/in/jonathan-trevino' className='hover:rotate-3'>
+      <Link target='_blank' href='https://linkedin.com/in/jonathan-trevino' className='hover:rotate-3'>
         <Linkedin />
       </Link>
-      <Link href='https://github.com/jonathantrevino' className='hover:rotate-3'>
+      <Link target='_blank' href='https://github.com/jonathantrevino' className='hover:rotate-3'>
         <Github />
       </Link>
-      <Link href='https://x.com/jtrevdev' className='hover:rotate-3'>
+      <Link target='_blank' href='https://x.com/jtrevdev' className='hover:rotate-3'>
         <Twitter />
       </Link >
     </div>
