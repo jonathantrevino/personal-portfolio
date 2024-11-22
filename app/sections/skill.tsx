d60151980e54ad1f151facd2a9599bcd0e88e39b
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-11-21 22:10:36 -0600

    adding dark mode for skill hover

--- app/sections/skill.tsx
+++ app/sections/skill.tsx
@@ -6,11 +6,11 @@ export const Skill = () => {
   return (
     <section>
       <h2 className='title'>Skills</h2>
-      <div className="relative flex flex-wrap sm:grid sm:grid-cols-4 md:grid-cols-8 lg:grid-cols-10  gap-2">
+      <div className="relative flex flex-wrap gap-2">
         {skills.map((skill, index) => (
           <div
             key={index}
-            className="sm:w-full sm:h-full w-[95px] h-[95px] aspect-square group relative bg-base-200 hover:scale-[1.05] transition-all"
+            className="w-[95px] h-[95px] aspect-square group relative bg-base-200 hover:scale-[1.05] transition-all"
           >
             <Image
               className="rounded-md transition-all ease-in-out group-hover:scale-[1.05] group-hover:z-10"
@@ -18,8 +18,12 @@ export const Skill = () => {
               alt={skill.title}
               fill
             />
-            <div className="group-hover:scale-100 scale-50 group-hover:opacity-100 group-hover:bg-gray-50 rounded-md px-2 opacity-0 absolute bottom-2 right-2 block shadow-lg border border-base-300 group-hover:z-20 sm:text-sm md:text-xs lg:text-sm transition-all">
-              {skill.title}
+            <div className='w-full h-[85px] relative sm:h-[95px] flex justify-center'>
+              <div className="group-hover:scale-100 scale-50 group-hover:opacity-100 group-hover:bg-[color:var(--skill-image)] rounded-md px-2 opacity-0 absolute bottom-2 block shadow-lg group-hover:z-20 text-xs md:text-sm  transition-all">
+                <div className='flex jusitfy-center text-center w-fit'>
+                  {skill.title}
+                </div>
+              </div>
             </div>
           </div>
         ))}
