4e651890a3b95f3943f0e5fcba74f25827718da7
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-11-21 22:11:55 -0600

    removing react svg errors

--- app/components/projectCard.tsx
+++ app/components/projectCard.tsx
@@ -43,7 +43,7 @@ export const ProjectCard = ({ title, description, image_url, skills, live, users
         </span>
         <span className='flex gap-2 h-fit overflow-visible'>{live &&
           <Link className='group overflow-visible hover:scale-[1.07] hover:rotate-3 transition-all ease-out' href={live}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-external-link">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link">
               <path className='' d="M15 3h6v6" /><path className='' d="M10 14 21 3" />
               <path className='' d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
             </svg>
--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -38,7 +38,7 @@ const Page: React.FC<{ params: { slug: string } }> = ({ params }) => {
               <h2 className='description'>{project.description}</h2>
             </span>
             <span className='flex gap-2 h-fit'>{project.live && <Link className='group overflow-visible hover:scale-[1.07] hover:rotate-3 transition-all ease-out' href={project.live}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-external-link">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link">
                 <path className='' d="M15 3h6v6" /><path className='' d="M10 14 21 3" />
                 <path className='' d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
               </svg>
