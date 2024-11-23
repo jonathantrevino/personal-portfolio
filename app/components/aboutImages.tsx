008fa3241c25663639b393dbd0295f1b6f5de0eb
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-11-23 00:16:08 -0600

    making photos animation play on inView

--- app/components/aboutImages.tsx
+++ app/components/aboutImages.tsx
@@ -5,9 +5,9 @@ import { motion } from 'motion/react'
 export const AboutImages = () => {
   return (
     <div className='flex justify-center items-center flex-[1] relative w-full md:w-[570px] h-full'>
-      <motion.img whileHover={{ top: 3, left: 4, rotate: '-3deg' }} src='/corne_keyboard.webp' className='h-fit absolute top-12 left-20' width={166} height={166} alt="jonathan trevino's headshot" />
+      <motion.img whileInView={{ rotate: '-3deg', top: 32, left: 90 }} src='/corne_keyboard.webp' className='h-fit absolute' width={166} height={166} alt="jonathan trevino's headshot" />
       <motion.img src='/jonathan_trevino_headshot.webp' className='h-fit absolute z-[2]' width={166} height={166} alt="jonathan trevino's headshot" />
-      <motion.img whileHover={{ bottom: 3, right: 4, rotate: 3 }} src='/gundam.webp' className='h-fit absolute bottom-12 right-20 z-[1]' width={166} height={166} alt="jonathan trevino's headshot" />
+      <motion.img whileInView={{ rotate: '3deg', bottom: 32, right: 90 }} src='/gundam.webp' className='h-fit absolute z-[1]' width={166} height={166} alt="jonathan trevino's headshot" />
     </div>
   )
 }
