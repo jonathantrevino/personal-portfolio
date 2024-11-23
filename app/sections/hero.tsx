65de1cb550024538b37d86203e05e835b3dee49b
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-11-23 00:16:28 -0600

    adding css class for paragraph

--- app/sections/hero.tsx
+++ app/sections/hero.tsx
@@ -8,7 +8,7 @@ export const Hero = () => {
         <h1>Jonathan Trevino</h1>
         <h2>Software engineer in web development</h2>
       </div>
-      <div className='max-w-[544px]'>UTSA Graduate, 2+ years experience developing websites that solve real problems, currently building webportfolios.dev</div>
+      <div className='max-w-[544px] text-less-important'>UTSA Graduate, 2+ years experience developing websites that solve real problems, currently building webportfolios.dev</div>
       <Socials />
     </section>
   )
