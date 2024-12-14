ecc3c7d92194cc26012e5c8e5126db1919d6d366
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-12-13 19:00:26 -0600

    getting unique categories

--- app/blogs/category/[category]/page.tsx
+++ app/blogs/category/[category]/page.tsx
@@ -1,6 +1,14 @@
 import CategoryRecents from "@/app/components/blogs/categoryRecents";
 import TopCategories from "@/app/components/blogs/topCategories";
 import { Spacer } from "@/app/components/spacer";
+import { getBlogPosts } from "../../utils";
+
+export async function generateStaticParams() {
+  let posts = getBlogPosts()
+
+  const uniqueCategories = Array.from(new Set(posts.map((post) => post.metadata.category)))
+  return uniqueCategories.map((category) => ({ category: String(category) }))
+}
 
 export default function Page({ params }: { params: { category: string } }) {
   return <div className='min-h-[calc(100vh-245.5px)]  mx-auto'>
