d4daf91794f38d4944bdc665adbdd7567c1b1192
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-12-16 14:27:16 -0600

    updating category view

--- app/components/blogs/categoryRecents.tsx
+++ app/components/blogs/categoryRecents.tsx
@@ -1,25 +1,13 @@
-import { getBlogPosts } from '@/app/blogs/utils';
-import Link from 'next/link';
+import { getBlogPosts } from '@/app/blog/utils';
 import React from 'react'
 import BlogPreview from './blogPreview';
 
 const CategoryRecents = ({ category }: { category: string }) => {
   const recents = getBlogPosts().filter((blog) => blog.metadata.category.includes(category));
 
-  if (recents.length === 0) {
-    return (
-      <div className='flex-[0.6] space-y-8'>
-        <h1 className='title'>Recently Published - <span className='italic'>Invalid Category</span></h1>
-        <div className='space-y-8'>
-          <p className='not-important text-center'>No articles found for this category</p>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div className='flex-[0.6] space-y-8'>
-      <h1 className='title'>Recently Published - {category}</h1>
+      <h1 className='text-[23px]'>Recently Published - {category.toLocaleUpperCase()} </h1>
       <div className='space-y-8'>
         {recents.sort((a, b) => {
           if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
