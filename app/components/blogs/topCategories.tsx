575a5c993954e004310da0b82a6eb88499b9af77
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-12-16 14:27:41 -0600

    updating route

--- app/components/blogs/topCategories.tsx
+++ app/components/blogs/topCategories.tsx
@@ -1,5 +1,4 @@
-import { getCategories } from '@/app/blogs/utils';
-import { topCategories } from '@/app/constants/categories'
+import { getCategories } from '@/app/blog/utils';
 import Link from 'next/link';
 import React from 'react'
 
@@ -11,7 +10,7 @@ const TopCategories = () => {
       <h2>Top Categories</h2>
       <div className='flex flex-wrap sm:grid  grid-cols-2 lg:grid-cols-3 gap-3'>
         {categories.map((category, index) =>
-          <Link key={index} className='col-span-1 p-2 text-center bg-[color:var(--container-color)] rounded-md hover:scale-[1.01] transition-all' role='button' href={`/blogs/category/${category}`}>{category}</Link>
+          <Link key={index} className='col-span-1 p-2 text-center bg-[color:var(--container-color)] rounded-md hover:scale-[1.01] transition-all' role='button' href={`/blog/${category}`}>{category}</Link>
         )}
       </div>
     </div>
