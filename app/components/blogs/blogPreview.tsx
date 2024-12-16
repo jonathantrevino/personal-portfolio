1ead448b44536ec29e6b0f05774f9e48b64e39eb
jonathantrevino <JonathanTrevino1327@yahoo.com>
2024-12-16 14:27:29 -0600

    updating route

--- app/components/blogs/blogPreview.tsx
+++ app/components/blogs/blogPreview.tsx
@@ -1,11 +1,11 @@
-import { formatDate } from '@/app/blogs/utils'
+import { formatDate } from '@/app/blog/utils'
 import Link from 'next/link'
 import React from 'react'
 
 const BlogPreview = ({ blog }: any) => {
   return (
     <article key={blog.slug} className=''>
-      <Link href={`/blogs/${blog.slug}`} className='space-y-3'>
+      <Link href={`/blog/${blog.metadata.category}/${blog.slug}`} className='space-y-3'>
         <h3 className='font-bold'>{blog.metadata.title}</h3>
         <p className='leading-relaxed'>{blog.metadata.summary}</p>
         <p className='not-important'>{formatDate(blog.metadata.publishedAt)}</p>
