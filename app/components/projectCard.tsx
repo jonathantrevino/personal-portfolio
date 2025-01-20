8970e13534c4ddf6149595c49d5f3c2cf730204c
jonathantrevino <yeahimjt+github@gmail.com>
2025-01-19 20:42:04 -0600

    adding stats

--- app/components/projectCard.tsx
+++ app/components/projectCard.tsx
@@ -3,7 +3,7 @@ import React, { useRef, useState } from 'react'
 import { motion, useScroll, useTransform } from "motion/react"
 import { ProjectCardType } from '../constants/project';
 import Link from 'next/link';
-import { MoveRight } from 'lucide-react';
+import { Flame, Star } from 'lucide-react';
 
 function useParallax(value: any, distance: number) {
   return useTransform(value, [0, 1], [-distance, distance]);
@@ -14,7 +14,7 @@ export const ProjectCard = ({ title, description, image_url, skills, live, users
   const [isClicked, setIsClicked] = useState(false);
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({ target: ref })
-  const y = useParallax(scrollYProgress, 35)
+  const y = useParallax(scrollYProgress, 5)
 
 
   // Handle button click and trigger animation
@@ -31,34 +31,43 @@ export const ProjectCard = ({ title, description, image_url, skills, live, users
       animate={isClicked ? 'clicked' : ''}
       transition={{ duration: 0.6, ease: [0.4, 0, 0.2, 1] }}
       onClick={handleClick}
-      className='md:aspect-video p-[30px] rounded-md relative project-card overflow-hidden transition-all space-y-5 border border-[color:var(--skill-card)]'
+      className='md:aspect-video p-[30px] rounded-md relative project-card overflow-hidden transition-all space-y-5 border border-[color:var(--skill-card)] group cursor-pointer'
     >
-      <div className='flex justify-between overflow-visible md:flex-row flex-col gap-3'>
-        <span>
-          <span className='text-gray-500 text-sm not-important'>{users}+ Users</span>
-          <h3 className='font-bold text-[23px]'>{title}</h3>
-          <p className=''>{description}</p>
-        </span>
-        <div className='flex gap-5 items-start'>
-          <span className='flex gap-2 h-fit overflow-visible'>{live &&
-            <Link className='group overflow-visible hover:scale-[1.07] hover:rotate-3 transition-all ease-out' href={live}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-external-link">
-                <path className='' d="M15 3h6v6" /><path className='' d="M10 14 21 3" />
-                <path className='' d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6" />
-              </svg>
-            </Link>}</span>
-          <div className=''>
-            <Link className='flex gap-2 self-end group' href={`/projects/${slug}`}>Read More<MoveRight className='group-hover:translate-x-1 transition-all' /></Link>
+      <Link href={`/projects/${slug}`} className='space-y-8 md:space-y-2'>
+
+        <div className='flex lg:flex-row flex-col lg:justify-between gap-5 gap-y-4'>
+          <span>
+            <h3 className='font-bold text-[23px]'>{title}</h3>
+            <p className=''>{description}</p>
+          </span>
+
+          <div className='flex gap-4 flex-wrap'>
+            <div className='rounded-lg'>
+              <h4 className='text-sm font-light'>Total Users</h4>
+              <span className='flex gap-2 items-center'>
+                <p className='text-2xl font-extrabold'>110+ Users</p>
+                <Star fill='orange' stroke='orange' />
+              </span>
+            </div>
+            <div className='rounded-lg'>
+              <h4 className='text-sm font-light'>Monthly Visitors</h4>
+              <span className='flex gap-2 items-center'>
+                <p className='text-2xl font-extrabold'>9,000</p>
+                <Flame fill='red' stroke='orange' />
+              </span>
+            </div>
+          </div>
+
+        </div>
+
+        <div className='flex gap-3 flex-wrap'>{skills && skills.map((skill) => <div key={skill} className='skill-card px-2 py-1 rounded-md'>{skill}</div>)}</div>
+        <div className='justify-center items-end flex'>
+          <div className='lg:relative lg:-bottom-24 md:absolute md:-bottom-[320px] -bottom-24 relative transition-all group-hover:rotate-[-14deg] group-hover:translate-x-32 duration-300'>
+            <motion.img style={{ y }} className='px-4' src={image_url} width={669} height={476} alt='webportfolios project preview' />
           </div>
         </div>
-      </div>
-      <div className='flex gap-3 flex-wrap'>{skills && skills.map((skill) => <div key={skill} className='skill-card px-2 py-1 rounded-md'>{skill}</div>)}</div>
-      <div className='justify-center items-end flex'>
-        <div className='lg:relative lg:-bottom-24 md:absolute md:-bottom-80 -bottom-24 relative transition-all'>
-          <motion.img style={{ y }} className='px-4' src={image_url} width={669} height={476} alt='webportfolios project preview' />
-        </div>
-      </div>
-    </motion.div>
+      </Link>
+    </motion.div >
   )
 }
 
