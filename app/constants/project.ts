2a61a0248be30e40c6c2b4844804a6742f8c605a
jonathantrevino <yeahimjt+github@gmail.com>
2025-01-19 21:06:23 -0600

    using webp

--- app/constants/project.ts
+++ app/constants/project.ts
@@ -29,7 +29,7 @@ export const project_card: ProjectCardType[] = [
 
     title: 'webportfolios.dev',
     description: 'Directory of Developer Portfolios',
-    image_url: '/webportfolios_homepage.png',
+    image_url: '/webportfolios_homepage.webp',
     skills: ['Next.js', 'Tailwind', 'Typescript', 'Firebase', 'AWS'],
     slug: 'webportfolios.dev',
     live: 'https://www.webportfolios.dev',
@@ -59,7 +59,7 @@ export const projects: ProjectType[] = [
   {
     title: 'webportfolios.dev',
     description: 'Directory of Developer Portfolios',
-    image_url: '/webportfolios_homepage.png',
+    image_url: '/webportfolios_homepage.webp',
     skills: ['Next.js', 'Tailwind', 'Typescript', 'Firebase', 'AWS'],
     slug: 'webportfolios.dev',
     live: 'https://www.webportfolios.dev',
