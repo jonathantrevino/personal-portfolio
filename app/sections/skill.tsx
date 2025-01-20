fc41b1426fb2967e544e0c4b6d9d36d68c7ce6d4
jonathantrevino <yeahimjt+github@gmail.com>
2025-01-19 21:09:57 -0600

    fixing format

--- app/sections/skill.tsx
+++ app/sections/skill.tsx
@@ -20,7 +20,7 @@ export const Skill = () => {
               draggable={false}
             />
             <div className='w-full  relative h-full flex justify-center z-[10]'>
-              <div className="group-hover:scale-100 scale-50 group-hover:opacity-100 group-hover:bg-[color:var(--skill-image)] rounded-md px-2 opacity-0 absolute bottom-2 block shadow-lg group-hover:z-20 text-xs md:text-sm  transition-all">
+              <div className="group-hover:scale-100 scale-50 group-hover:opacity-100 group-hover:bg-[color:var(--skill-image)] rounded-md px-2 opacity-0 absolute bottom-1 block shadow-lg group-hover:z-20 text-xs md:text-sm  transition-all">
                 <div className='flex jusitfy-center text-center w-fit z-[10]'>
                   {skill.title}
                 </div>
