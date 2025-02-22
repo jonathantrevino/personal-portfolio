4c1ab39f6e2ede56ab28a23a4b624aeb9870aee9
jonathantrevino <yeahimjt+github@gmail.com>
2025-02-22 00:01:36 -0600

    removing latest tags

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,25 +1,15 @@
 import React from 'react'
-import Recents from '../components/blogs/recents'
 import { Spacer } from '../components/spacer'
-import TopCategories from '../components/blogs/topCategories'
-
-export function generateMetadata() {
-  return {
-    title: 'Blog',
-    description: 'Personal blog where I share my insights, experiences, and tips on web development, building projects, and my journey as a developer.',
-  }
-}
+import SideNav from '../components/sideNav'
+import { Project } from '../sections/project'
+import { Hero } from '../sections/hero'
+import Recents from '../components/blogs/recents'
 
 const page = () => {
   return (
-    <div className='min-h-[calc(100vh-245.5px)]  mx-auto'>
+    <div className="">
       <Spacer />
-      <div className='flex md:flex-row flex-col-reverse gap-5'>
-        <Recents />
-        <div className='flex-[0.4]'>
-          <TopCategories />
-        </div>
-      </div>
+      <Recents />
     </div>
   )
 }
